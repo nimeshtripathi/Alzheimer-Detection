b911641f4df7316efb366f345ea54b64bee2aaa7
Nimesh Tripathi <55591288+nimeshtripathi@users.noreply.github.com>
2021-05-18 03:41:04 -0500

    Update DT.py

--- Machine Learning/Decision Tree/DT.py
+++ Machine Learning/Decision Tree/DT.py
@@ -1,1 +1,12 @@
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+
+data = pd.read_csv('Oasis_longitudinal.csv')
+
+accuracy_list = []
+opt_score = 0
+opt_var=2.0
+folds_no = 10 
+
+aa= DecisionTreeClassifier() 
